f61b6d13ec6b57d3a3f33106baed854ec8c1d22a
lucifer <imvk009@gmail.com>
2023-08-12 13:11:13 +0530

    @Repository Correction

--- src/main/java/com/spring/boot/rest/api/dao/CourseRepository.java
+++ src/main/java/com/spring/boot/rest/api/dao/CourseRepository.java
@@ -4,7 +4,6 @@ import com.spring.boot.rest.api.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
--- src/main/java/com/spring/boot/rest/api/entity/Course.java
+++ src/main/java/com/spring/boot/rest/api/entity/Course.java
@@ -17,4 +17,5 @@ public class Course {
     private String courseName;
     private String description;
 
+
 }
